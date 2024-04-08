54a086380b51c836e567f8208091f80166479d55
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-04-08 09:00:52 +0200

    Contenido centrado en la ventana

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 </script>
 
 <template>
+    <h1>Title</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <button class="btn btn-primary">Get Started</button>
 </template>
 
 <style scoped>
